fc2cad920d947d6b6b78d9dc6397eb50cd74ea24
RaphaelCastro98 <raphaelbarrosmelo@gmail.com>
2025-12-10 12:03:42 -0300

    Exception handling - delete

--- src/main/java/br/com/raphaelbarros/finalorder/resourses/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/raphaelbarros/finalorder/resourses/exceptions/ResourceExceptionHandler.java
@@ -12,13 +12,24 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err= new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> Database(DatabaseException e, HttpServletRequest request) {
+		String error = "resource not found";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
 
 }
--- src/main/java/br/com/raphaelbarros/finalorder/services/UserService.java
+++ src/main/java/br/com/raphaelbarros/finalorder/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.raphaelbarros.finalorder.entities.User;
 import br.com.raphaelbarros.finalorder.repositories.UserRepository;
+import br.com.raphaelbarros.finalorder.resourses.exceptions.DatabaseException;
 import br.com.raphaelbarros.finalorder.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User udpate(Long id, User obj) {
