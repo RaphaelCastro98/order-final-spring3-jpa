fe6af67dd06e5b914b65f6486e01c6d5b60ba5a6
RaphaelCastro98 <raphaelbarrosmelo@gmail.com>
2025-12-10 12:14:18 -0300

    Exception handling - update

--- src/main/java/br/com/raphaelbarros/finalorder/services/UserService.java
+++ src/main/java/br/com/raphaelbarros/finalorder/services/UserService.java
@@ -12,6 +12,7 @@ import br.com.raphaelbarros.finalorder.entities.User;
 import br.com.raphaelbarros.finalorder.repositories.UserRepository;
 import br.com.raphaelbarros.finalorder.resourses.exceptions.DatabaseException;
 import br.com.raphaelbarros.finalorder.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -45,9 +46,13 @@ public class UserService {
 	}
 	
 	public User udpate(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e ) {
+			throw new ResourceNotFoundException(e.getMessage());
+		}
 	}
 
 	private void updateData(User entity, User obj) {
